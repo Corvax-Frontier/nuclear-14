2f22064ce56f8d6aa2aaf18c8736a1df27ab5ae8
Kill_Me_I_Noobs <118206719+Vonsant@users.noreply.github.com>
2024-07-02 22:01:24 +0300

    Update HTNSystem.cs

--- Content.Server/NPC/HTN/HTNSystem.cs
+++ Content.Server/NPC/HTN/HTNSystem.cs
@@ -2,15 +2,15 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using Content.Server.Administration.Managers;
+using Robust.Shared.CPUJob.JobQueues;
+using Robust.Shared.CPUJob.JobQueues.Queues;
 using Content.Server.NPC.HTN.PrimitiveTasks;
 using Content.Server.NPC.Systems;
 using Content.Shared.Administration;
 using Content.Shared.Mobs;
 using Content.Shared.NPC;
 using JetBrains.Annotations;
-using Robust.Server.GameObjects;
-using Robust.Shared.CPUJob.JobQueues;
-using Robust.Shared.CPUJob.JobQueues.Queues;
+using Robust.Server.GameObjects; // Corvax
 using Robust.Shared.Player;
 using Robust.Shared.Prototypes;
 using Robust.Shared.Utility;
